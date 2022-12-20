4a86f7ac44c3a6d5e2cd9ca0e40ca3e856cebf58
marcipanis <yuliya-mazko@yandex.ru>
2022-12-20 12:13:32 +0300

    [shrks-28] Some fix

--- packages/client/src/app/pages/Start/Start.tsx
+++ packages/client/src/app/pages/Start/Start.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../Layout/Layout";
 import styles from './Start.module.scss'
-import Button from "../../components/core/Button/Button";
+import Button, {ButtonVariant} from "../../components/core/Button/Button";
 
 import {useNavigate} from "react-router-dom";
 
@@ -12,22 +12,19 @@ function Start() {
     }
 
     return (
-        <Layout gradient>
-            <div className={styles.game_over_container_wrap}>
-                <div className={styles.start_container}>
-                    <div className={styles.wrap_for_start_icon_container}>
-                        <div className={styles.start_icon_container}></div>
+        <Layout withGradient>
+            <div className={styles['start-container-wrap']}>
+                <div className={styles['start-container']}>
+                    <div className={styles['wrap-for-start-icon-container']}>
+                        <div className={styles['start-icon-container']}></div>
                     </div>
 
-                    <div className={styles.wrap_for_start_button}>
-                        <Button onClick={navigateToStartHandler} className={styles.start_button_play}>Старт</Button>
-                        <Button onClick={navigateToStartHandler} className={styles.start_button_exit}>Выход</Button>
+                    <div className={styles['wrap-for-start-button']}>
+                        <Button onClick={navigateToStartHandler} variant={ButtonVariant.primary} className={styles['start-button-play']}>Старт</Button>
+                        <Button onClick={navigateToStartHandler} variant={ButtonVariant.tertiary} className={styles['start-button-exit']}>Выход</Button>
                     </div>
                 </div>
             </div>
-
-
-
         </Layout>
     );
 }
